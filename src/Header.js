23c8c12e9030c5419c3181be948d09a6383313e6
dk2684 <mingsonle@gmail.com>
2022-12-07 14:39:24 -0800

    First commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.css"
+
+function Header() {
+    return (
+        <div className="header___main">
+            <h1>Cat Generator</h1>
+        </div>
+    )
+}
+
+export default Header
